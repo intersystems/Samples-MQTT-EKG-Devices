8073fc528f0ebdf62a73b854e4dfb791caf3c103
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2021-11-19 14:27:13 +0900

    add external server

---
+++ dotnet/MyLibrary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using InterSystems.Data.IRISClient.ADO;
+using InterSystems.Data.IRISClient.Gateway;
+
+namespace dc
+{
+    public class MyLibrary
+    {
+        public string s1="abc";
+
+        public int DoSomething()
+        {
+            return 123;
+        }
+
+    }
+}
