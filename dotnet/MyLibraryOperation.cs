740eb88f0a92c3f73813e8f3f60d4fded7260d48
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2021-11-29 09:06:20 +0000

    more

---
+++ dotnet/MyLibraryOperation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using InterSystems.Data.IRISClient.Gateway;
+using InterSystems.Data.IRISClient.ADO;
+
+namespace dc
+{
+    public class MyLibraryOperation : InterSystems.EnsLib.PEX.BusinessOperation
+    {
+
+        public override void OnTearDown() { } // Abstract method in PEX superclass. Must override.
+        public override void OnInit() { } // Abstract method in PEX superclass. Must override.
+
+
+        public override object OnMessage(object request)
+        {
+            long seqno;
+
+            LOGINFO("Message Received");
+            IRISObject req = (IRISObject)request;
+            LOGINFO("Received object: " + req.InvokeString("%ClassName", 1));
+
+            String value = req.GetString("StringValue");
+            LOGINFO("Received StringValue: " + value);
+
+            // Decode mqttmsg (raw data) into rows. It depends on how they are encoded.
+            //
+            // ++Write your code here++
+            int rowcount = 2000;
+            int columncount = 4;
+
+            int[] array = new int[rowcount];
+            for (int i = 0; i < rowcount; i++)
+            {
+                array[i] = i;
+            }
+            // --Write your code here--
+
+            IRIS iris = GatewayContext.GetIRIS();
+            // Native API
+            // Save decoded values into IRIS via Native API
+            seqno = (long)iris.ClassMethodLong("Solution.RAWDATA", "GETNEWID");
+            for (int i = 0; i < rowcount; i += columncount)
+            {
+                iris.ClassMethodStatusCode("Solution.RAWDATA", "INSERT", seqno, array[i], array[i + 1], array[i + 2], array[i + 3]);
+            }
+
+            return null;
+
+        }
+
+    }
+}
---
+++ dotnet/MyLibraryService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using InterSystems.Data.IRISClient.Gateway;
+using InterSystems.Data.IRISClient.ADO;
+
+namespace dc
+{
+    public class MyLibraryService : InterSystems.EnsLib.PEX.BusinessService
+    {
+        public string TargetConfigNames;
+
+        public override void OnTearDown() { } // Abstract method in PEX superclass. Must override.
+        public override void OnInit() { } // Abstract method in PEX superclass. Must override.
+
+        public override object OnProcessInput(object request)
+        {
+            long seqno;
+
+            LOGINFO("Message Received");
+            IRISObject req = (IRISObject)request;
+            LOGINFO("Received object: " + req.InvokeString("%ClassName", 1));
+
+            String value = req.GetString("StringValue");
+            LOGINFO("Received StringValue: " + value);
+
+
+            IRIS iris = GatewayContext.GetIRIS();
+            seqno = (long)iris.ClassMethodLong("Solution.RAWDATA", "GETNEWID");
+            IRISObject newrequest = (IRISObject)iris.ClassMethodObject("Ens.StringContainer", "%New", seqno);
+
+            // Iterate through target business components and send request message
+            string[] targetNames = TargetConfigNames.Split(',');
+            foreach (string name in targetNames)
+            {
+                SendRequestAsync(name, newrequest);
+                LOGINFO("Target:" + name);
+
+            }
+            return null;
+        }
+
+    }
+}
